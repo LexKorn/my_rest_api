68682d21962429b9c20066caac3293a40cb3e862
Lex <fischer84@mail.ru>
2022-04-25 09:08:03 +0300

    first release

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,41 @@
 const express = require('express');
 const path = require('path');
 const colors = require('colors');
+const {v4} = require('uuid');
 
 const app = express();
 
-const CONTACTS = [
-    { id: 1, name: 'Alex', value: '154654696', marked: false }
+let CONTACTS = [
+    { id: v4(), name: 'Alex', value: '154654696', marked: false }
 ];
 
+app.use(express.json());   // midlleware для работы с response
 
+//endpoints
+//GET
+app.get('/api/contacts', (req, res) => {
+    res.status(200).json(CONTACTS);
+});
+
+//POST
+app.post('/api/contacts', (req,res) => {   // т.к. REST API, то route тот же самый, что и в GET запросе
+    const contact = {...req.body, id: v4(), marked: false};
+    CONTACTS.push(contact);
+    res.status(201).json(contact);
+});
+
+//DELETE
+app.delete('/api/contacts/:id', (req, res) => {
+    CONTACTS = CONTACTS.filter(item => item.id !== req.params.id);
+    res.status(200).json({message: "Contact was deleted"});
+});
+
+//PUT
+app.put('/api/contacts/:id', (req, res) => {
+    const idx = CONTACTS.findIndex(item => item.id === req.params.id);    
+    CONTACTS[idx] = req.body;
+    res.json(CONTACTS[idx]);
+})
 
 
 
